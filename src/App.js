b36f07eafbb9f47c03a5c7c112073c98b9609d04
DESKTOP-P2N46VN\hyeon <kkul2000@naver.com>
2024-11-01 22:17:36 +0900

    ~#7.5

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import { useEffect, useState } from "react";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  const moneyChange = (event) => {
-    setMoney(event.target.value);
-  };
-
   return (
-    <div>
-      <h1>The Coins! ({loading ? null : coins.length})</h1>
-      <input
-        onChange={moneyChange}
-        type="number"
-        placeholder="$"
-        value={money}
-      ></input>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD /{" "}
-              {(money / coin.quotes.USD.price).toFixed(5)} {coin.symbol}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
